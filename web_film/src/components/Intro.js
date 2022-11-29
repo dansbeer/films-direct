1602f47c862ceb9ce78b62be75850f250d0e4746
Pandya Arya Daniswara <mrsterbern@gmail.com>
2022-11-29 15:22:26 +0700

    First

---
+++ web_film/src/components/Intro.js
@@ -0,0 +1,22 @@
+import { Button, Col, Container, Row } from "react-bootstrap"
+
+
+const Intro = () => {
+    return (
+        <div className="intro">
+            <Container className="text-white d-flex justify-content-center text-center align-items-center">
+                <Row>
+                    <Col>
+                        <div className="title">NONTON GRATIS</div>
+                        <div className="title">GAK BIKIN DOMPET TIPIS</div>
+                        <div className="introButton mt-4 text-center">
+                            <Button variant="dark">Lihat Semua List</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    )
+}
+
+export default Intro
---
+++ web_film/src/components/NavigationBar.js
@@ -0,0 +1,22 @@
+import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap"
+
+
+const NavigationBar = () => {
+    return (
+        <div>
+        <Navbar variant="dark">
+            <Container>
+                <NavbarBrand href="/">
+                    FILMS
+                </NavbarBrand>
+                <Nav>
+                    <Nav.Link href="#trending">TRENDING</Nav.Link>
+                    <Nav.Link href="#superhero">SUPERHERO</Nav.Link>
+                </Nav>
+                </Container>
+        </Navbar>
+        </div>
+    )
+}
+
+export default NavigationBar
